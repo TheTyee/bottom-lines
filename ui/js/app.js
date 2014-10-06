2f81e961ceee52c53d49792dc73001af29d544ce
Phillip Smith <ps@phillipadsmith.com>
2014-10-06 13:03:06 -0400

    Adding requirement for various libraries, and roughing in the UI views

--- ui/js/app.js
+++ ui/js/app.js
@@ -3,9 +3,119 @@
 //= require jquery.js
 //= require modernizr.js
 //= require bootstrap.js
-// require jquery.slimscroll.min.js
 // require jquery.easings.min.js
+// require jquery.slimscroll.js
 // require jquery.fullPage.js
 //= require underscore.js
 //= require backbone.js
-//= require d3.js
+//= require backbone.layoutmanager.js
+//= require jquery.smooth-scroll.js
+//= require TweenMax.js
+//= require jquery.scrollmagic.js
+// require jquery.scrollmagic.debug.js
+//= require jquery.fluidbox.js
+// require d3.js
+
+
+// 
+// ===================================================================
+// Configuration
+// ===================================================================
+window.App = {};
+
+Backbone.Layout.configure({
+    manage: true,
+});
+
+// ===================================================================
+// Utilities
+// ===================================================================
+
+// ===================================================================
+// Models
+// ===================================================================
+
+// Countdown timer
+
+
+// ===================================================================
+// Collections
+// ===================================================================
+
+App.dataCards = {}; 
+App.articles = {}; // Sections
+App.articleSlides = {}; // Fullscreen header slides
+
+// ===================================================================
+// Views
+// ===================================================================
+
+App.articleView = {}; // Articles
+
+App.slideView = Backbone.View.extend({ // Article header slides
+    events: {
+        'click a.smooth':  "smoothMofo",
+    },
+    initialize: function (options) {
+        var id = options.id;
+        this.$el = $(id); 
+        $('a').smoothScroll({
+            speed: 1000,
+        });
+    },
+    smoothMofo: function(e) {
+        console.log( e );
+    }
+});
+
+App.cardView = {}; // Data cards
+
+
+// ===================================================================
+// Layouts
+// ===================================================================
+
+App.Layout = new Backbone.Layout({
+    // Attach the Layout to the main container.
+    el: "body",
+    views: {
+        "fallingGrades": new App.slideView({id: '#falling-grades'}),
+        "airAndWater": new App.slideView({id: '#air-and-water'}),
+    },
+    afterRender: function() {
+    }
+});
+
+// ===================================================================
+// Router
+// ===================================================================
+
+App.Router = Backbone.Router.extend({
+    initialize: function() { 
+    },
+    routes: {
+        '': 'start',
+        '*default': 'defaultRoute'
+    },
+    start: function() {
+        console.log('started');
+    },
+    defaultRoute: function() {
+        console.log("404");
+    }
+});
+
+// Start the app
+App.router = new App.Router();
+Backbone.history.start();
+
+
+// Move this into Layout?
+var duration = $(".parallax").height() + $(window).height();
+var bgPosMovement = "0 " + (duration*0.8) + "px";
+// init controller
+var controller = new ScrollMagic({globalSceneOptions: {triggerHook: "onEnter", duration: duration}});
+// build scenes
+new ScrollScene({triggerElement: ".parallax"})
+.setTween(TweenMax.to(".parallax", 1, {backgroundPosition: bgPosMovement, ease: Linear.easeNone}))
+.addTo(controller);
