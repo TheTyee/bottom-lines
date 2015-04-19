9266e5d2379cb4fcbe7bedbef80d70a4b2c4097d
Phillip Smith <ps@phillipadsmith.com>
2015-04-19 15:07:06 -0700

    Update base/tile layer

--- ui/js/natural-security.js
+++ ui/js/natural-security.js
@@ -2,7 +2,12 @@
 
 function showMap(data) {
     var map = L.map(data.dom).setView([data.lon, data.lat], data.zoom);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.{ext}', {
+                type: 'sat',
+                ext: 'jpg',
+                attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency',
+                subdomains: '1234'
+        }).addTo(map);
 }
+
+
