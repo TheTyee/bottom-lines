641ae21ee109b680b184b34c08ff8ba42513cc9e
Phillip Smith <ps@phillipadsmith.com>
2015-05-09 16:55:37 -0700

    Change base layer for natural security slides

--- ui/js/natural-security.js
+++ ui/js/natural-security.js
@@ -2,12 +2,7 @@
 
 function showMap(data) {
     var map = L.map(data.dom).setView([data.lon, data.lat], data.zoom);
-        L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.{ext}', {
-                type: 'sat',
-                ext: 'jpg',
-                attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency',
-                subdomains: '1234'
-        }).addTo(map);
+       L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}).addTo(map);
 }
-
-
